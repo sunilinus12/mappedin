d02ef4e009c1077885eb5c3792adee4ad48475e6
sunil jaunwal <sunil@hipster-inc.com>
2024-07-10 11:17:52 +0530

    updated for floating labels

--- App.tsx
+++ App.tsx
@@ -9,21 +9,40 @@ const options = {
   clientSecret: 'RJyRXKcryCMy4erZqqCbuB1NbR66QTGNXVE0x3Pg6oCIlUR1',
   venue: 'mappedin-demo-mall',
   perspective: 'Website',
+  labelAllLocationsOnInit: false, // do disable the location labels
 };
 
 const App: React.FC = () => {
   const mapView = useRef<MapViewStore>(null);
-
   return (
     <SafeAreaView style={{flex: 1}}>
       <MiMapView
         style={{flex: 1}}
+        onFirstMapLoaded={() => {
+          mapView.current?.FloatingLabels.labelAllLocations({
+            interactive: true,
+          });
+        }}
         key="mappedin"
         options={options}
         ref={mapView}
-        onClick={({polygons}) => {
-          if (polygons.length > 0) {
-            mapView.current?.setPolygonColor(polygons[0], '#BF4320');
+        // onClick={({polygons}) => {
+        //   if (polygons.length > 0) {
+        //     mapView.current?.setPolygonColor(polygons[0], '#BF4320');
+        //   }
+        // }}
+        onClick={({floatingLabels}) => {
+          console.log('floatingLabels:', floatingLabels);
+
+          if (floatingLabels && floatingLabels.length > 0) {
+            const polygon = floatingLabels[0].node.polygon;
+            console.log('Setting color for polygon:', polygon);
+
+            mapView.current?.setPolygonColor(polygon, 'red');
+          } else {
+            console.log('Clearing all polygon colors');
+
+            mapView.current?.clearAllPolygonColors();
           }
         }}
       />
